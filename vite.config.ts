912c19dd500985bf8fc527b841e077f00c9ed027
huguini123 <huguini123@gmail.com>
2025-01-20 04:09:26 +0000

    Configura projeto para deploy no Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/HugFoot25/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js'
-      }
-    }
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
+    sourcemap: true
   }
 }); 
