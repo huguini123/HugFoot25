e2658cfd427aeb7872ecfd7bed689b6faf4b36d8
huguini123 <huguini123@gmail.com>
2025-01-20 03:44:54 +0000

    Reconfigura projeto para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,42 +2,12 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Hugfoot25/',
+  base: '/HugFoot25/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/js/[name].js',
-        chunkFileNames: 'assets/js/[name].[hash].js',
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
-            return 'assets/images/[name].[hash][extname]';
-          }
-          if (ext === 'css') {
-            return 'assets/css/[name].[hash][extname]';
-          }
-          return 'assets/[name].[hash][extname]';
-        }
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: 'localhost',
-    open: true
+    outDir: 'dist'
   }
 }); 
